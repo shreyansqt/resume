03bc10e7125e2d9b9422c7db25c104add7ab9e7e
Shreyans Jain <shreyans.jain@onelogic.de>
2023-12-12 15:01:58 +0100

    update general description

--- data/general.ts
+++ data/general.ts
@@ -1,13 +1,15 @@
 export const generalData = {
   name: "Shreyans Jain",
   avatar: "https://github.com/shreyansqt.png",
-  jobTitle: "Senior Front-End Developer",
+  jobTitle: "Freelance Front-End Developer",
   website: "https://shreyans.co",
   about: `With 10+ years of experience as a Senior Front End Engineer, I have developed a deep passion for creating exceptional user experiences through innovative and responsive web design. My skills in HTML, CSS, JavaScript, and frontend frameworks like VueJS and ReactJS allow me to deliver exceptional results on every project.
 
 As an out-of-the-box problem solver and versatile engineer, I excel at collaborating with cross-functional teams to deliver projects on time and within budget. My meticulous attention to detail ensures that every line of code is carefully crafted for maximum performance, scalability, and maintainability.
 
-If you're looking for a Senior Front End Engineer who is passionate about staying up-to-date with the latest frontend technologies and delivering high-quality, detail-oriented work, then let's connect and explore how I can help bring your vision to life.`,
+If you're looking for a Senior Front End Engineer who is passionate about staying up-to-date with the latest frontend technologies and delivering high-quality, detail-oriented work, then let's connect and explore how I can help bring your vision to life.
+
+Currently, I am looking for a frontend contract role with Vue 3 or React as base library, with a focus on UX. Startup atmosphere in a greenfield project is preferred.`,
   contacts: [
     {
       label: "Email",
